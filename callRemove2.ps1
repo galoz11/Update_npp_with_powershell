84121793fb516b93fe503733081d1eb68879e58e
avi <tt@tt.com>
2021-08-22 11:24:03 +0300

    fix names

--- callRemove2.ps1
+++ callRemove2.ps1
@@ -1,5 +1,5 @@
 write-host
-write-host i am call2.ps1 and calling to RemoveAndExlude.ps1
+write-host i am call2.ps1 and calling to callRemove3.ps1
 write-host
 
 # . .\RemoveAndExlude1.ps1 -pa2 "wordPa1" -pa1 "wordPa2"
@@ -7,7 +7,7 @@ write-host
 $pFolder = "D:\myScripts\Powershell\ttt"
 
 Try{. .\callRemove3.ps1 -pFolder D:\myScripts\Powershell\ttt -pa1 "wordPa2" -ErrorAction Stop}
-Catch{Write-Warning "The File: RemoveAndExlude.ps1 couldn't be found.."; exit 71}
+Catch{Write-Warning "The File: callRemove3.ps1 couldn't be found.."; exit 71}
 
 
 write-host mmm $data[2]
